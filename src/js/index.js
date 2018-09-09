f7abd5c0cb98ea7c045c052a63bf77fe50258a5d
zoltankiss85 <nartak85@gmail.com>
2018-09-10 01:24:22 +0200

    some finished section

--- src/js/index.js
+++ src/js/index.js
@@ -4,12 +4,21 @@ import LandingBg from '../images/landing-bg.png';
 import ServicesTBg from '../images/services-title.png';
 import PackagesBg from '../images/packages-bg.png';
 import TestimonialsTBg from '../images/testimonials-title.png'
+import ContactBg from '../images/contact-bg.png';
 import IconDesign from '../images/ikon_design.svg';
 import IconServer from '../images/ikon_szerver.svg';
 import IconCustom from '../images/ikon_személyreszabott.svg';
 import IconAnalitycs from '../images/ikon_analitycs.svg';
 import IconFast from '../images/ikon_gyors.svg';
 import IconPlans from '../images/ikon_plans.svg';
+import ArrowDown from '../images/arrow-down.svg';
+import ArrowLeft from '../images/arrow-left.svg';
+import ArrowRight from '../images/arrow-right.svg';
+import Customer1 from '../images/customer.png';
+import CustomerPh1 from '../images/customer-placeholder1.png';
+import CustomerPh2 from '../images/customer-placeholder2.png';
+
+import Stars from '../images/stars.svg';
 
 
 $(document).ready(function() {
